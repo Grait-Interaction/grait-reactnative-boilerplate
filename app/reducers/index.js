fa4b2e3d9df457d5be6c9dce1958c5b8f7f4f95c
Albin Hubsch <albin.hubsch@gmail.com>
2018-03-04 17:58:32 +0100

    Removed nav from combined redusers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,7 +3,6 @@ import storage from 'redux-persist/es/storage'
 import immutableTransform from 'redux-persist-transform-immutable'
 
 // Import reducers
-import nav from './nav'
 import test from './test'
 import user from './user'
 
@@ -11,13 +10,12 @@ const config = {
     transforms: [immutableTransform()],
     key: 'root',
     storage: storage,
-    blacklist: ['nav'],
+    // blacklist: ['user'],
     // whitelist: ['user', 'eagles'], // Uncomment to use whitelist
 }
 
 // Paste reducers to be included in the store here
 export default persistCombineReducers(config, {
-    nav: nav,
     test: test,
     // user: user, // Uncomment to use user reducer
 })
