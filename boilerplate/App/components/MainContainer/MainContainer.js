133cc3a2ed09108eaa874b036063b2c2466f01da
Albin Hubsch <albin.hubsch@gmail.com>
2018-07-28 15:45:20 +0200

    Fixed action template

--- boilerplate/App/components/MainContainer/MainContainer.js
+++ boilerplate/App/components/MainContainer/MainContainer.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
 
-const MainContainer = ({ children }) => {
+const MainContainer = ({ children, style }) => {
     return (
-        <SafeAreaView style={{ flex: 1 }} forceInset={{ top: 'always', bottom: 'always' }}>
+        <SafeAreaView style={[{flex: 1}, style ? style : null]} forceInset={{ top: 'always', bottom: 'always' }}>
             {children}
         </SafeAreaView>
     )
